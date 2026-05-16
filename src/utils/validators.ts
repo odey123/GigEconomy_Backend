9596e299ecacc76ec93bbe724c1166d72430d01e
Jude Odey <ohotujude@gmail.com>
2026-05-16 02:20:45 +0100

    fix: allow empty beneficiaryAccount in wallet creation

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -111,7 +111,7 @@ export const walletValidationSchemas = {
     dateOfBirth: Joi.string().required(), // mm/dd/yyyy
     gender: Joi.string().valid('1', '2').required(), // '1' = Male, '2' = Female
     address: Joi.string().required().min(5),
-    beneficiaryAccount: Joi.string().length(10).optional(),
+    beneficiaryAccount: Joi.string().length(10).optional().allow(''),
   }),
 
   withdraw: Joi.object({
